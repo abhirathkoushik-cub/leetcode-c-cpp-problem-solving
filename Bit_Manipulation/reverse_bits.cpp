31e824c98fbfdc6190450e17fa71a4ca4216f689
abhirathkoushik-cub <abhirath.koushik@colorado.edu>
2025-06-12 00:03:08 -0600

    adding bit manipulation folder with initial reverse_bits program

---
+++ Bit_Manipulation/reverse_bits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void reverse_bits(uint32_t* num)
+{
+    uint32_t reversed=0;
+    while(*num>0)
+    {
+        reversed = reversed << 1; // Moving the bit position (in the answer variable) to be modified to the left each time. 
+        reversed = reversed | ((*num)&1); // *num&1 checks if *num is set at that position. | operation sets the bit in the answer variable. 
+        *num = *num>>1; // In each interation, *num is shifted to the right. As the result is stored in another variable, this can be done to *num.
+    }
+    cout<<"Reversed:"<<reversed<<endl;
+}
+
+int main()
+{
+    uint32_t var = 129;
+    // uint32_t var = 5;
+    reverse_bits(&var);
+    return 0;
+}
