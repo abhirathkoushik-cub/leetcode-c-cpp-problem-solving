08e986e5834f370d1b90519ce2451fe81af82b1b
abhirathkoushik-cub <abhirath.koushik@colorado.edu>
2025-07-16 13:10:40 -0600

    notes after whiteboard coding

--- Bit_Manipulation/lc_reverse_bits.cpp
+++ Bit_Manipulation/lc_reverse_bits.cpp
@@ -10,6 +10,7 @@ void reverse_bits(uint32_t* num)
     uint32_t reversed=0;
     while(*num>0)
     {
+        /* FISH SET. FIrst SHift then SET. No branching req to check if 0 or 1 before setting. Set for ALL. */
         reversed = reversed << 1; // Moving the bit position (in the answer variable) to be modified to the left each time. 
         reversed = reversed | ((*num)&1); // *num&1 checks if *num is set at that position. | operation sets the bit in the answer variable. 
         *num = *num>>1; // In each interation, *num is shifted to the right. As the result is stored in another variable, this can be done to *num.
