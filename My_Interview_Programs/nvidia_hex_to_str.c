2dadd1b2cc824cc8d8bdb0ebde1e46705825ded2
abhirathkoushik-cub <abhirath.koushik@colorado.edu>
2025-07-16 14:57:09 -0600

    imp a-f ascii conversion inclusion after whiteboard coding

--- My_Interview_Programs/nvidia_hex_to_str.c
+++ My_Interview_Programs/nvidia_hex_to_str.c
@@ -4,18 +4,28 @@ Convert from a Hex to a String without using any in-built functions
 */
 
 #include<stdio.h>
+#include<ctype.h> // Used for tolower()
 
 int main()
 {
-    int num=0x20250630;
+    int num=0x2025ABCD;
 
     char temp_str[10]; 
     int i=0;
     while(num>0)
     {
-        /* Using ASCII method of converting from an integer to a character.
-        0x0000000F mask ensures that only the LSB integer is considered each time */
-        temp_str[i]= '0' + (num&0x0000000F); 
+        // 0x0000000F mask ensures that only the LSB integer is considered each time
+        int nibble = num&0x0000000F;
+
+        if(nibble < 10) // IMP: If digit(0-9) then ASCII conversion from int to char is '0' + nibble
+        {
+            temp_str[i]= '0' + nibble; 
+        }
+        else
+        {
+            temp_str[i] = (tolower(nibble)) + 'a' - 10; // IMP: If (a-f) of Hex, then ASCII conversion to char is nibble + 'a' - 10
+        }
+        
         num = num >> 4; // Shifting by 4 as this is a 32-bit integer, so each digit is a 4-bit shift
         i++;
     }
