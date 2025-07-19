195eda8a078d02dec57d276cce8e692467b5bda8
abhirathkoushik-cub <abhirath.koushik@colorado.edu>
2025-07-19 13:03:50 -0600

    optimized code using BK method for counting bits

--- Bit_Manipulation/lc_counting_bits.cpp
+++ Bit_Manipulation/lc_counting_bits.cpp
@@ -17,9 +17,18 @@ public:
             int j=i; // As the value of 'i' gets modified in while loop, copying the value to a new number 'j'
             while(j!=0)
             {
+                /* Optimized Approach: Brian Kernighan's Method
+                    n & (n-1) Clears the Lowest Set Bit or Least Significant Set Bit (LSSB)
+                    As each iteration removes 1 set bit, this loop only runs for as many set bits in the number,
+                    not for every bit in the number as the Previous Approach. */
+                j &= (j-1);
+                count++;
+
+                /* Previous Approach: Check only if rightmost bit is set and keep moving the bits to the right
                 if((j&1)==1) // Using '&' operation to check if the rightmost bit is set
                     count++;
                 j=j>>1; // Using '>>' operator to keep moving the bits of the number to the right
+                */
             }
             ans.push_back(count);
         }
